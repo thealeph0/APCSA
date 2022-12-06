c8bd452429ea4e1e83a74a7c4c13c1c86e31d0db
thealeph0 <84290656+thealeph0@users.noreply.github.com>
2022-12-06 07:08:56 -0800

    Create GUIGrades.java

---
+++ FinalProjects/GraderGUI/GUIGrades.java
@@ -0,0 +1,29 @@
+/* The driver class below uses
+   the Grade class to convert
+   grades entered by the user
+   into other forms of the same
+   grade. The results are shown
+   in a GUI, as is the input.
+*/
+
+import javax.swing.JOptionPane;
+
+public class Grades
+{
+	public static void main(String[] args)
+	{
+		String origLetter = JOptionPane.showInputDialog("Please enter Letter Grade:");
+    
+		//include string for origNumeric
+		
+		Grade userGrades = new Grade(origLetter, Double.parseDouble(origNumeric));
+    
+    //displays the final result
+		
+		JOptionPane.showMessageDialog(null,
+				"Numeric Grade Equivalent = " + userGrades.getNewNumeric() +
+						"\n\nLetter Grade Equivalent = " + userGrades.getNewLetter(),
+				"Grades",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}
